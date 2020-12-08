b5bfd8c471fc5cb9a9c8bd6c2b56aeba9ff5ab21
Elisabetta Manca <elisabetta.manca@cern.ch>
2020-12-08 16:38:24 +0100

    new version of helpers, implementation of systematics internally, convert boost histograms to np arrays and save to hdf5 file

--- framework/header.py
+++ framework/header.py
@@ -10,24 +10,24 @@ def getValueType(obj):
     open_br, close_br = class_name.find('<'), class_name.rfind('>')
     value_type = class_name[open_br+1:close_br]
     return value_type
+ROOT.gInterpreter.Declare("""
+#include <boost/histogram.hpp>
+using namespace boost::histogram;
+using boost_histogram = boost::histogram::histogram<std::vector<boost::histogram::axis::variable<> >, boost::histogram::storage_adaptor<std::vector<boost::histogram::accumulators::weighted_sum<>, std::allocator<boost::histogram::accumulators::weighted_sum<> > > > >;
 
-# ROOT.gInterpreter.Declare("""
-# #include <boost/histogram.hpp>
-# using namespace boost::histogram;
-# using boost_histogram = boost::histogram::histogram<std::vector<boost::histogram::axis::variable<> >, boost::histogram::storage_adaptor<std::vector<boost::histogram::accumulators::weighted_sum<>, std::allocator<boost::histogram::accumulators::weighted_sum<> > > > >;
-
-# std::vector<std::vector<float>> convert(boost_histogram& h){ 
-#     std::vector<float> vals;
-#     std::vector<float> sumw2;
-#     for (auto&& x : indexed(h)) {
-#         const auto n = x->value();
-#         const auto w2 = x->variance();
-#         vals.emplace_back(n);
-#         sumw2.emplace_back(w2);
-#         }
-#     return std::vector<std::vector<float>>{vals,sumw2};
-#     }; 
-# """
-# )
+std::vector<std::vector<float>> convert(boost_histogram& h){ 
+    std::vector<float> vals;
+    std::vector<float> sumw2;
+    for (auto&& x : indexed(h)) {
+        const auto n = x->value();
+        const auto w2 = x->variance();
+        //std::cout<< n <<std::endl;
+        vals.emplace_back(n);
+        sumw2.emplace_back(w2);
+        }
+    return std::vector<std::vector<float>>{vals,sumw2};
+    }; 
+"""
+)
 
 
